43cb2acee42d51c6c4fa3bc6525149768390ab01
sudhandhakshneswaran <sudhandhakshneswaran@gmail.com>
2016-11-19 14:52:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bg1.mp4'));
 });
 
